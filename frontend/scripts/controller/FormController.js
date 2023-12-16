b75aa8e2cd5d2d67a8be0785828aac044c2c381d
SzDora <szlucska.dora@gmail.com>
2023-12-16 11:43:13 +0100

    valid form

--- frontend/scripts/controller/FormController.js
+++ frontend/scripts/controller/FormController.js
@@ -1,7 +1,9 @@
 import FormView from "../view/FormView.js";
 
-export default class FormController{
-constructor(parent){
-    new FormView(parent)
-}
+export default class FormController {
+  constructor(ds, parent, desc) {
+    this.ds = ds;
+    this.form = new FormView(parent, desc);
+ 
+  }
 }
--- frontend/scripts/view/FormView.js
+++ frontend/scripts/view/FormView.js
@@ -1,57 +1,49 @@
 export default class FormView {
-  #formData={}
-  constructor(parent) {
+  #formData = {};
+
+  constructor(parent, desc) {
     parent.html("<form class='align-items-center m-auto bg-transparent'>");
     this.formElem = parent.find("form");
-    this.makeHTML();
+    this.makeHTML(desc);
+    this.getInfoForm();
   }
 
-  makeHTML() {
-    let txt = `<div class="mb-4 m-auto">
-          <input
-            type="text"
-            id="title"
-            placeholder="Cím"
-            name="title"
-            class="form-control"
-          />
-        </div>
-        <div class="mb-4 m-auto">
-          <input
-            type="number"
-            class="form-control"
-            id="evaluation"
-            value="0.0"
-            min="0.0"
-            max="10.0"
-            step="0.1"
-            name="evaluation"
-          />
-        </div>
-        <div class="mb-4 m-auto">
-          <textarea rows="15" class="w-100"></textarea>
-        </div>
-        <div class="m-auto w-50">
-          <input
-            type="submit"
-            id="submit"
-            value="Küld"
-            class="btn btn-success w-100"
-          />
-        </div>`;
+  makeHTML(desc) {
+    let txt = "";
+    for (const k in desc) {
+      const e = desc[k];
+      switch (e.type) {
+        case "text":
+          txt += `<div class="mb-4 m-auto"><input id="${e.id}" class="${e.class}" placeholder="${e.placeholder}"/></div>`;
+          break;
+        case "number":
+          txt += `<div class="mb-4 m-auto"><input id="${e.id}" class="${e.class}" value="${e.value}" min="${e.min}" max="${e.max}" step="${e.step}"/></div>`;
+          break;
+        case "textarea":
+          txt += `<div class="mb-4 m-auto"><textarea id="${e.id}" class="${e.class}" placeholder="${e.placeholder}" rows="${e.rows}"></textarea></div>`;
+          break;
+      }
+    }
+    txt += `<div class="m-auto w-50"><input type="submit" id="submit" value="Küld" class="btn btn-success w-100"/></div>`;
     this.formElem.append(txt);
   }
-  getInfoForm(){
-    this.submitElem = this.formElem.find("#submit")
-    this.titleElem =this.formElem.find("#title")
-    this.evaElem = this.formElem.find("#evaluation")
-    this.textArea = this.formElem.find("textarea")
-    this.submitElem.on("click", (event)=>{
+
+  getInfoForm() {
+    this.submitElem = this.formElem.find("#submit");
+    this.titleElem = this.formElem.find("#title");
+    this.evaElem = this.formElem.find("#evaluation");
+    this.textArea = this.formElem.find("textarea");
+    this.submitElem.on("click", (event) => {
       event.preventDefault();
-      this.#formData.title=this.titleElem.val()
-      this.#formData.evaulation = this.evaElem.val()
-      this.#formData.description = this.textArea.val()
-      console.log(this.#formData)
-    })
+      this.#formData.title = this.titleElem.val();
+      this.#formData.evaulation = this.evaElem.val();
+      this.#formData.description = this.textArea.val();
+      this.trigger("postSubmit");
+    });
+  }
+
+  trigger(e) {
+    const event = new CustomEvent(e, { detail: this.#formData });
+    window.dispatchEvent(event);
   }
 }
