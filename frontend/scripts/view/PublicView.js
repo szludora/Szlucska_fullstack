2d2fa66736937b3b15b1f6d60ac268c251c2f52e
szludora <szlucska.dora@gmail.com>
2024-01-08 20:15:59 +0100

    képek méretezése, javítása

--- frontend/scripts/view/PublicView.js
+++ frontend/scripts/view/PublicView.js
@@ -17,8 +17,9 @@ export default class PublicView {
       let txt = `<div class="p-1 col-sm-6 col-md-4 col-lg-3"><div class="konyv bg-light rounded p-2 m-4 " style="position: relative">`;
           txt += `<div class="cim fs-4">${e.title}</div>`;
           txt += `<div class="ertekeles fs-6 position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">${e.evaluation}</div>`;
-          txt += `<div class="leiras mt-4 fs-6 fst-italic mb-5">${e.description}</div>`;
-      txt += `<button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;Kosárba</button></div></div>`;
+          txt += `<div class="leiras mt-2 fs-6 fst-italic mb-2">${e.description}</div>`;
+          txt += `<div class="kep mb-3"><img src="../../imgs/books/${e.id}.jpg"></div>`
+          txt += `<button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;Kosárba</button></div></div>`;
       this.divElem.append(txt);
     }
   }
