45b34ef6e8ff866769d8f75c2374e51f9b5446c8
szludora <szlucska.dora@gmail.com>
2024-01-08 20:22:19 +0100

    értékelés színe

--- frontend/scripts/view/PublicView.js
+++ frontend/scripts/view/PublicView.js
@@ -4,22 +4,32 @@ export default class PublicView {
   constructor(list, parent) {
     this.parent = parent;
     this.list = list;
-    console.log(list)
-    parent.html(`<div class="row"></div>`)
-    console.log(parent)
-    this.divElem = this.parent.find(".row")
+    console.log(list);
+    parent.html(`<div class="row"></div>`);
+    console.log(parent);
+    this.divElem = this.parent.find(".row");
     this.makeHTML(list);
   }
 
   makeHTML(list) {
+    let bg = ``;
     for (const k in list) {
       const e = list[k];
       let txt = `<div class="p-1 col-sm-6 col-md-4 col-lg-3"><div class="konyv bg-light rounded p-2 m-4 " style="position: relative">`;
-          txt += `<div class="cim fs-4">${e.title}</div>`;
-          txt += `<div class="ertekeles fs-6 position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">${e.evaluation}</div>`;
-          txt += `<div class="leiras mt-2 fs-6 fst-italic mb-2">${e.description}</div>`;
-          txt += `<div class="kep mb-3"><img src="../../imgs/books/${e.id}.jpg"></div>`
-          txt += `<button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;Kosárba</button></div></div>`;
+      txt += `<div class="cim fs-4">${e.title}</div>`;
+      if (e.evaluation < 4) {
+        bg = `bg-danger`;
+      } else {
+        if (e.evaluation < 7) {
+          bg = `bg-secondary`;
+        } else {
+          bg = `bg-success`;
+        }
+      }
+      txt += `<div class="ertekeles fs-6 position-absolute top-0 start-100 translate-middle badge rounded-pill ${bg}">${e.evaluation}</div>`;
+      txt += `<div class="leiras mt-2 fs-6 fst-italic mb-2">${e.description}</div>`;
+      txt += `<div class="kep mb-3"><img src="../../imgs/books/${e.id}.jpg"></div>`;
+      txt += `<button type="button" class="btn btn-outline-primary"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;Kosárba</button></div></div>`;
       this.divElem.append(txt);
     }
   }
